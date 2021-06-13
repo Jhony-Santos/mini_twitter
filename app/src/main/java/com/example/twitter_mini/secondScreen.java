4dcfd7c6d0cb0b1562f737f89b8af0eb7adb4042
jntd <jntd@gft.com>
2021-06-13 16:26:22 -0300

    Layout ajustado

---
+++ app/src/main/java/com/example/twitter_mini/secondScreen.java
@@ -0,0 +1,14 @@
+package com.example.twitter_mini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class secondScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_screen);
+    }
+}
